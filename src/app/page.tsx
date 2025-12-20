f85c8bdd64a575a9f504bddcaf99a69d90b85ba3
tofu-dev0123 <m.komukai0123@gmail.com>
2025-12-21 00:09:15 +0900

    tailwindの設定を修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <p>学びと個人開発を綴るブログ</p>
       <h1>tofu blog</h1>
+      <Button>ボタン</Button>
     </div>
   );
 }

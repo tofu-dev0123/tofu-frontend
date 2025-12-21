f6ffbe13fa36e7d5cf0f58f10cc129ce6c618006
tofu-dev0123 <m.komukai0123@gmail.com>
2025-12-21 11:46:27 +0900

    typeチェックの設定を追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,22 +3,31 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import prettier from "eslint-plugin-prettier";
 import prettierConfig from "eslint-config-prettier";
+import importPlugin from "eslint-plugin-import";
 
 export default defineConfig([
   ...nextVitals,
   ...nextTs,
 
-  // Prettier を ESLint に統合
   {
     plugins: {
       prettier,
+      import: importPlugin,
     },
     rules: {
+      // ../ を禁止（alias はOK）
+      "no-restricted-imports": [
+        "error",
+        {
+          patterns: ["../*"],
+        },
+      ],
+
       "prettier/prettier": "error",
+      "@typescript-eslint/no-explicit-any": "error",
     },
   },
 
-  // ESLint と Prettier の競合ルールを無効化
   prettierConfig,
 
   globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),

679c9e5c826b3745a2986c480c8f25327cf58f8c
tofu-dev0123 <m.komukai0123@gmail.com>
2025-12-22 14:18:24 +0900

    エラーメッセージのレイアウトを修正

--- src/components/features/admin/common/ErrorMessage.tsx
+++ src/components/features/admin/common/ErrorMessage.tsx
@@ -3,7 +3,7 @@ import { AlertCircle } from 'lucide-react';
 function ErrorMessage({ errorMessage }: { errorMessage: string[] }) {
   return (
     <>
-      <div className="flex justify-center items-center mx-6 my-4 bg-white rounded-md p-2">
+      <div className="flex flex-col mx-6 my-4 bg-white rounded-md p-2 gap-2">
         {errorMessage.map((message) => (
           <div key={message} className="flex items-center gap-2 text-xs">
             <AlertCircle className="w-4 h-4 flex-shrink-0 text-red-600" />

beef331e3ea4a1780aee62bb1180d4ce8d615a12
tofu-dev0123 <m.komukai0123@gmail.com>
2026-01-05 23:12:42 +0900

    suspenseでラップするように修正

--- src/app/admin/(dashboard)/posts/page.tsx
+++ src/app/admin/(dashboard)/posts/page.tsx
@@ -1,5 +1,17 @@
+import { Suspense } from 'react';
 import PostListMain from '@/components/features/admin/posts/PostListMain';
+import { Spinner } from '@/components/ui/spinner';
 
 export default function Page() {
-  return <PostListMain />;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex items-center justify-center h-full w-full">
+          <Spinner className="size-8" />
+        </div>
+      }
+    >
+      <PostListMain />
+    </Suspense>
+  );
 }

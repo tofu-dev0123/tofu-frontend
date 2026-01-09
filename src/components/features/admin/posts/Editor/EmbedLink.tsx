db96995e831e748222036e4355abdd39143bf373
tofu-dev0123 <m.komukai0123@gmail.com>
2026-01-09 14:13:44 +0900

    埋め込みリンクの修正

--- src/components/features/admin/posts/Editor/EmbedLink.tsx
+++ src/components/features/admin/posts/Editor/EmbedLink.tsx
@@ -20,43 +20,45 @@ function EmbedLink({ open, onClose }: EmbedLinkProps) {
   if (!open) return null;
 
   return (
-    <Card
-      className="fixed lg:absolute w-80 h-12 flex items-center justify-start border border-gray-200 shadow p-2 lg:translate-x-[10%] lg:translate-y-[-50%]"
-      style={
-        isMobile
-          ? {
-              top: '30%',
-              left: '50%',
-              transform: 'translate(-50%, -50%)',
-            }
-          : {
-              top: state.cursorPosition.y,
-              left: state.cursorPosition.x,
-            }
-      }
-    >
-      <Input
-        type="url"
-        placeholder="URLを入力してください"
-        className="w-full shadow-none border-none"
-        value={state.inputUrl}
-        onChange={actions.handleInputChange}
-      />
-      <Button
-        className="w-12 h-8 cursor-pointer mx-2"
-        onClick={actions.handleInsert}
+    <div className="fixed top-0 left-0 w-full h-full z-50" onClick={onClose}>
+      <Card
+        className="fixed lg:absolute w-80 h-12 flex items-center justify-start border border-gray-200 shadow p-2 lg:translate-x-[10%] lg:translate-y-[-50%]"
+        style={
+          isMobile
+            ? {
+                top: '30%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+              }
+            : {
+                top: state.cursorPosition.y,
+                left: state.cursorPosition.x,
+              }
+        }
       >
-        挿入
-      </Button>
-      <Image
-        src={addGreyIcon}
-        alt="追加"
-        width={15}
-        height={15}
-        className="hover:cursor-pointer hover:opacity-60 duration-200 rotate-45 absolute -top-1 -right-1"
-        onClick={onClose}
-      />
-    </Card>
+        <Input
+          type="url"
+          placeholder="URLを入力してください"
+          className="w-full shadow-none border-none"
+          value={state.inputUrl}
+          onChange={actions.handleInputChange}
+        />
+        <Button
+          className="w-12 h-8 cursor-pointer mx-2"
+          onClick={actions.handleInsert}
+        >
+          挿入
+        </Button>
+        <Image
+          src={addGreyIcon}
+          alt="追加"
+          width={15}
+          height={15}
+          className="hover:cursor-pointer hover:opacity-60 duration-200 rotate-45 absolute -top-1 -right-1"
+          onClick={onClose}
+        />
+      </Card>
+    </div>
   );
 }
 

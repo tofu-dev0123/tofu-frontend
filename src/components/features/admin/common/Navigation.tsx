172b1ab06eb3e73111aba045b5b7ab4ef03f05f0
tofu-dev0123 <m.komukai0123@gmail.com>
2026-01-09 22:11:45 +0900

    ナビゲーションのスタイルを修正

--- src/components/features/admin/common/Navigation.tsx
+++ src/components/features/admin/common/Navigation.tsx
@@ -28,31 +28,15 @@ function Navigation({ handleClickLogout }: NavigationProps) {
   return (
     <nav className="w-full lg:w-15 h-20 px-4 flex lg:flex-col justify-center items-center mx-auto fixed bottom-5 lg:top-[50%] lg:translate-y-[-50%] lg:left-20 lg:translate-x-[-50%] z-50">
       <div className="lg:w-15 mx-auto bg-white flex lg:flex-col justify-center items-center gap-15 shadow-lg border border-gray-300/50 rounded-full lg:px-2 lg:py-8 bg-white relative py-4 px-10">
-        {/* アニメーション背景 */}
-        {activeIndex >= 0 && !isMobile && (
-          <div
-            className="absolute bottom-0 left-0 w-10 h-10 bg-gray-200 rounded-full transition-transform duration-300 ease-in-out z-0"
-            style={{
-              transform: isMobile
-                ? `translateX(${14 + activeIndex * 60}px)`
-                : `translateY(${22 + activeIndex * 60}px)`,
-              top: isMobile ? '50%' : '0',
-              left: isMobile ? '0' : '50%',
-              marginTop: isMobile ? '-20px' : '0',
-              marginLeft: isMobile ? '0' : '-20px',
-            }}
-          />
-        )}
-
         {/* ナビゲーションアイコン */}
-        {NAVIGATION_ITEMS.map((item) => (
+        {NAVIGATION_ITEMS.map((item, index) => (
           <Image
             key={item.path}
             src={item.icon}
             alt={item.alt}
             width={20}
             height={20}
-            className="cursor-pointer hover:opacity-60"
+            className={`cursor-pointer hover:scale-110 duration-300 ${activeIndex === index ? 'opacity-100' : 'opacity-40'}`}
             onClick={() => handleIconClick(item.path)}
           />
         ))}
@@ -63,7 +47,7 @@ function Navigation({ handleClickLogout }: NavigationProps) {
           alt="logout"
           width={20}
           height={20}
-          className="cursor-pointer hover:opacity-60"
+          className="cursor-pointer opacity-40 hover:scale-110 duration-300 hover:opacity-100"
           onClick={handleClickLogout}
         />
       </div>

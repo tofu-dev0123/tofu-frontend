cc97356cdc541fecc1f485b9c8bb2c366ebe7b83
tofu-dev0123 <m.komukai0123@gmail.com>
2026-01-09 21:53:42 +0900

    ナビゲーションのレイアウトを修正

--- src/components/features/admin/common/Navigation.tsx
+++ src/components/features/admin/common/Navigation.tsx
@@ -3,21 +3,17 @@
 import Image from 'next/image';
 import { usePathname, useRouter } from 'next/navigation';
 import logoutIcon from '@/assets/images/logout-icon.png';
-import useHeader from '@/hooks/admin/common/useHeader';
 import {
   NAVIGATION_ITEMS,
   getActiveIndex,
 } from '@/constants/admin/navigationItem';
 import useCheckMobile from '@/hooks/admin/common/useCheckMobile';
+
 interface NavigationProps {
   handleClickLogout?: () => void;
-  keyword?: string;
-  handleInputChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  handleSearch?: () => void;
 }
 
 function Navigation({ handleClickLogout }: NavigationProps) {
-  const { handleClickLogo } = useHeader();
   const pathname = usePathname();
   const router = useRouter();
   const isMobile = useCheckMobile();
@@ -30,59 +26,46 @@ function Navigation({ handleClickLogout }: NavigationProps) {
   };
 
   return (
-    <nav className="w-full h-full py-2 flex lg:flex-col justify-start items-center">
-      {/* ロゴ */}
-      <div className="flex items-center cursor-pointer gap-4 p-2">
-        <span
-          className="text-2xl font-bold cursor-pointer"
-          onClick={handleClickLogo}
-        >
-          Tofu Blog
-        </span>
-      </div>
+    <nav className="w-full h-20 px-4 flex lg:flex-col justify-center items-center mx-auto fixed bottom-5 z-50">
+      <div className="lg:w-15 mx-auto bg-white flex lg:flex-col justify-center items-center gap-15 shadow-lg border border-gray-300/50 rounded-full lg:px-2 lg:py-8 bg-white relative py-4 px-10">
+        {/* アニメーション背景 */}
+        {activeIndex >= 0 && !isMobile && (
+          <div
+            className="absolute bottom-0 left-0 w-10 h-10 bg-gray-200 rounded-full transition-transform duration-300 ease-in-out z-0"
+            style={{
+              transform: isMobile
+                ? `translateX(${14 + activeIndex * 60}px)`
+                : `translateY(${22 + activeIndex * 60}px)`,
+              top: isMobile ? '50%' : '0',
+              left: isMobile ? '0' : '50%',
+              marginTop: isMobile ? '-20px' : '0',
+              marginLeft: isMobile ? '0' : '-20px',
+            }}
+          />
+        )}
 
-      {/* ナビゲーションアイコン */}
-      <div className="flex flex-1 justify-center items-center w-full p-2">
-        <div className="lg:w-15 mx-auto flex lg:flex-col justify-center items-center gap-10 shadow-lg border border-gray-300/50 rounded-full lg:px-2 lg:py-8 bg-white relative px-6 py-4">
-          {/* アニメーション背景 */}
-          {activeIndex >= 0 && (
-            <div
-              className="absolute top-0 left-0 w-10 h-10 bg-gray-200 rounded-full transition-transform duration-300 ease-in-out z-0"
-              style={{
-                transform: isMobile
-                  ? `translateX(${14 + activeIndex * 60}px)`
-                  : `translateY(${22 + activeIndex * 60}px)`,
-                top: isMobile ? '50%' : '0',
-                left: isMobile ? '0' : '50%',
-                marginTop: isMobile ? '-20px' : '0',
-                marginLeft: isMobile ? '0' : '-20px',
-              }}
-            />
-          )}
-
-          {/* ナビゲーションアイコン */}
-          {NAVIGATION_ITEMS.map((item) => (
-            <Image
-              key={item.path}
-              src={item.icon}
-              alt={item.alt}
-              width={20}
-              height={20}
-              className="cursor-pointer hover:opacity-60 duration-200 z-10"
-              onClick={() => handleIconClick(item.path)}
-            />
-          ))}
-
-          {/* ログアウトアイコン */}
+        {/* ナビゲーションアイコン */}
+        {NAVIGATION_ITEMS.map((item) => (
           <Image
-            src={logoutIcon}
-            alt="logout"
+            key={item.path}
+            src={item.icon}
+            alt={item.alt}
             width={20}
             height={20}
-            className="cursor-pointer hover:opacity-60 duration-200"
-            onClick={handleClickLogout}
+            className="cursor-pointer hover:opacity-60"
+            onClick={() => handleIconClick(item.path)}
           />
-        </div>
+        ))}
+
+        {/* ログアウトアイコン */}
+        <Image
+          src={logoutIcon}
+          alt="logout"
+          width={20}
+          height={20}
+          className="cursor-pointer hover:opacity-60"
+          onClick={handleClickLogout}
+        />
       </div>
     </nav>
   );

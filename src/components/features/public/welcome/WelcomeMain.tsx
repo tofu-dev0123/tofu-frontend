d86fb549b975d20ba7492c4d7caba5ede5fedb28
tofu-dev0123 <m.komukai0123@gmail.com>
2026-01-12 14:29:17 +0900

    レスポンシブ対応

--- src/components/features/public/welcome/WelcomeMain.tsx
+++ src/components/features/public/welcome/WelcomeMain.tsx
@@ -21,22 +21,22 @@ function WelcomeMain() {
           animate={{ opacity: 1 }}
           className="h-screen w-full relative overflow-hidden"
         >
-          <div className="h-full w-full flex ">
-            <div className="h-full w-1/2 flex flex-col justify-center">
+          <div className="h-full w-full lg:flex ">
+            <div className="h-full lg:w-1/2 w-full flex flex-col justify-center">
               <div>
                 <motion.p
                   initial={{ opacity: 0 }}
                   animate={{ opacity: 1 }}
                   transition={{ duration: 2, delay: 3.5 }}
-                  className="font-grotesk font-bold text-xl tracking-wider mr-10 pl-20 text-right "
+                  className="font-grotesk font-bold lg:text-xl text-sm tracking-wider mr-10 pl-20 text-right "
                 >
                   Welcome to
                 </motion.p>
                 <motion.h1
-                  initial={{ opacity: 0, x: -100 }}
+                  initial={{ opacity: 0, x: -20 }}
                   animate={{ opacity: 1, x: 0 }}
                   transition={{ duration: 2, delay: 1 }}
-                  className="mr-10 font-logo text-[140px] font-bold text-right leading-none pb-10"
+                  className="mr-10 font-logo lg:text-[140px] text-[4rem] font-bold text-right leading-none lg:pb-7 pb-3"
                 >
                   Tofu blog
                 </motion.h1>
@@ -44,7 +44,7 @@ function WelcomeMain() {
                   initial={{ opacity: 0 }}
                   animate={{ opacity: 1 }}
                   transition={{ duration: 2, delay: 3.5 }}
-                  className="font-grotesk font-bold text-xl tracking-wider mr-10 pl-20 text-right"
+                  className="font-grotesk font-bold lg:text-xl text-sm tracking-wider mr-10 pl-20 text-right"
                 >
                   develop and learning and growing
                 </motion.p>
@@ -54,15 +54,15 @@ function WelcomeMain() {
                   initial={{ opacity: 0 }}
                   animate={{ opacity: 1 }}
                   transition={{ duration: 2, delay: 3.5 }}
-                  className="rounded-none text-public-main border-2 bg-black font-grotesk p-2 font-bold hover:cursor-pointer hover:bg-public-main hover:text-black transition-all duration-300"
+                  className="rounded-none text-lg lg:text-xl text-sm tracking-wider text-public-main border-2 bg-black font-grotesk p-2 font-bold hover:cursor-pointer hover:bg-public-main hover:text-black transition-all duration-300"
                   onClick={handleStart}
                 >
                   Start reading
                 </motion.button>
               </div>
             </div>
-            <div className="h-full w-1/2 relative">
-              <motion.div className="absolute top-0 left-0 translate-x-[-5%] w-full h-full scale-140">
+            <div className="h-full lg:w-1/2 w-full lg:relative ">
+              <motion.div className="absolute top-0 left-0 lg:translate-x-[-5%] w-full h-full lg:scale-140">
                 <Image
                   src={tofuIconImage}
                   alt="welcome"

ba2f4afbf6a8ae3df5738a70044685c68399a13c
tofu-dev0123 <m.komukai0123@gmail.com>
2026-01-14 22:30:47 +0900

    モバイル版のレイアウト修正

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -4,16 +4,18 @@ import React from 'react';
 import PublicHeader from '@/components/features/public/common/PublicHeader';
 import usePageNavigation from '@/hooks/public/common/usePageNavigation';
 import { motion } from 'framer-motion';
+import useIsMobile from '@/hooks/public/common/useIsMobile';
 
 function PublicLayout({ children }: { children: React.ReactNode }) {
   const { isTop, isActive } = usePageNavigation();
+  const isMobile = useIsMobile();
 
   return (
     <div className="w-full h-screen bg-public-main fixed inset-0 overflow-auto flex flex-col">
       <motion.header
         className="w-full h-15"
         animate={{
-          marginTop: isTop ? 120 : 0, // lg:mt-30 相当（pxで制御）
+          marginTop: isTop ? (isMobile ? 0 : 120) : 0, // lg:mt-30 相当（pxで制御）
         }}
         transition={{
           duration: 1,

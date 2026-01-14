e24cb89f26aabedc89dff96736ac26e93f68ec81
tofu-dev0123 <m.komukai0123@gmail.com>
2026-01-14 17:01:42 +0900

    レスポンシブ対応

--- src/app/maintenance/page.tsx
+++ src/app/maintenance/page.tsx
@@ -5,16 +5,16 @@ import Link from 'next/link';
 export default function Page() {
   return (
     <div className="w-full h-screen flex flex-col relative">
-      <div className="w-1/2 h-[70%] absolute top-1/2 left-1/2 -translate-y-1/2">
+      <div className="lg:w-1/2 w-full h-[70%] absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 lg:translate-x-0">
         <BackGroundImage />
       </div>
       <header className="w-full h-15 lg:mt-30">
-        <div className="lg:pl-[10%] flex lg:items-end items-center justify-start lg:ml-0 ml-2">
+        <div className="h-full lg:pl-[10%] flex lg:items-end items-center justify-start lg:ml-0 ml-2">
           <Logo />
         </div>
       </header>
-      <div className="w-1/2 flex flex-col items-start justify-center lg:mr-0 mr-2 translate-y-[90px] lg:pl-[10%]">
-        <p className="text-lg font-sub-logo font-semibold">
+      <div className="lg:w-1/2 w-full flex flex-col items-start justify-center lg:mr-0 mr-2 translate-y-[90px] lg:pl-[10%] pl-4">
+        <p className="lg:text-lg text-md font-sub-logo font-semibold">
           ただいまメンテナンス中です...
         </p>
         <div className="flex justify-center items-start mt-10">
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,19 +5,19 @@ import Link from 'next/link';
 export default function NotFound() {
   return (
     <div className="w-full h-screen flex flex-col relative">
-      <div className="w-1/2 h-[70%] absolute top-1/2 left-1/2 -translate-y-1/2">
+      <div className="lg:w-1/2 w-full h-[70%] absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 lg:translate-x-0">
         <BackGroundImage />
       </div>
       <header className="w-full h-15 lg:mt-30">
-        <div className="lg:pl-[10%] flex lg:items-end items-center justify-start lg:ml-0 ml-2">
+        <div className="h-full lg:pl-[10%] flex lg:items-end items-center justify-start lg:ml-0 ml-2">
           <Logo />
         </div>
       </header>
-      <div className="w-1/2 flex flex-col items-start justify-center lg:mr-0 mr-2 translate-y-[90px] lg:pl-[10%]">
-        <p className="text-[110px] font-sub-logo font-semibold translate-y-[30px]">
+      <div className="lg:w-1/2 w-full flex flex-col items-start justify-center lg:mr-0 mr-2 translate-y-[90px] lg:pl-[10%] pl-4">
+        <p className="lg:text-[110px] text-[80px] font-sub-logo font-semibold lg:translate-y-[30px] translate-y-[20px]">
           404
         </p>
-        <p className="text-lg font-sub-logo font-semibold">
+        <p className="lg:text-lg text-md font-sub-logo font-semibold">
           お探しのページが見つかりませんでした。
         </p>
         <div className="flex justify-center items-start mt-10">

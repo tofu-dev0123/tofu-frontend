8fa55b9aa9651399c81d869a02cde896cd956133
tofu-dev0123 <m.komukai0123@gmail.com>
2026-01-15 22:06:30 +0900

    検索ボタン実装

--- src/components/features/public/blogs/BlogsList.tsx
+++ src/components/features/public/blogs/BlogsList.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Search from '@/components/features/public/blogs/Search';
 
 function BlogsList() {
+  const handleSearch = (value: string) => {
+    console.log(value);
+  };
+
   return (
     <div className="h-full lg:w-180 w-full px-2 flex flex-col py-10 mx-auto">
+      <Search handleSearch={handleSearch} />
       bloglist
     </div>
   );
---
+++ src/components/features/public/blogs/Search.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+import Image from 'next/image';
+import searchIcon from '@/assets/images/search-icon.png';
+import cancelIcon from '@/assets/images/cancel-icon.png';
+
+interface SearchProps {
+  handleSearch: (value: string) => void;
+}
+
+export default function Search({ handleSearch }: SearchProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="w-full h-10">
+      <motion.div
+        animate={{ width: isOpen ? 300 : 40 }}
+        transition={{ duration: 0.5 }}
+        className="h-full rounded-full border border-black/10 flex items-center justify-end"
+      >
+        <motion.div
+          className="w-full items-center justify-start gap-2"
+          animate={{
+            opacity: isOpen ? 1 : 0,
+            width: isOpen ? 260 : 0,
+            display: isOpen ? 'flex' : 'none',
+          }}
+          transition={{ duration: 0.5 }}
+        >
+          <Image
+            src={searchIcon}
+            alt="cancel"
+            width={14}
+            height={14}
+            className="opacity-50 ml-2 hover:opacity-100 cursor-pointer transition-all duration-300"
+            onClick={() => handleSearch('')}
+          />
+          <input
+            type="text"
+            placeholder="Search"
+            className="h-full flex-1 text-sm outline-none border-none"
+          />
+        </motion.div>
+        <div
+          className="w-5 h-5 flex items-center justify-center m-2"
+          onClick={handleClick}
+        >
+          <Image
+            src={isOpen ? cancelIcon : searchIcon}
+            alt="search"
+            width={14}
+            height={14}
+            className="opacity-50 hover:opacity-100 cursor-pointer transition-all duration-300"
+          />
+        </div>
+      </motion.div>
+    </div>
+  );
+}

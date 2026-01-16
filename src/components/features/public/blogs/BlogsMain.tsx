897eaceeebca7d6e15bc40fc4c1bf571c264d440
tofu-dev0123 <m.komukai0123@gmail.com>
2026-01-16 16:45:48 +0900

    該当する記事がない時の表示を実装

--- src/components/features/public/blogs/BlogsMain.tsx
+++ src/components/features/public/blogs/BlogsMain.tsx
@@ -6,19 +6,27 @@ import Blog from '@/components/features/public/blogs/Blog';
 import Keyword from '@/components/features/public/blogs/Keyword';
 
 function BlogsMain() {
-  const { blogsList, keyword } = useBlogsList();
+  const { blogsList, keyword, totalCount } = useBlogsList();
 
   return (
     <div className="h-full w-full px-2">
       <Search />
       <Keyword keyword={keyword} />
-      <div className="flex-1 overflow-y-auto py-4">
-        {blogsList.map((blog, index) => (
-          <div key={blog.post_id}>
-            <Blog blog={blog} index={index} />
-          </div>
-        ))}
-      </div>
+      {totalCount === 0 ? (
+        <div className="w-full flex items-center py-4">
+          <p className="text-sm text-gray-500 font-sub-logo">
+            条件に一致する記事はありませんでした...
+          </p>
+        </div>
+      ) : (
+        <div className="flex-1 overflow-y-auto py-4">
+          {blogsList.map((blog, index) => (
+            <div key={blog.post_id}>
+              <Blog blog={blog} index={index} />
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/lib/utils/dateFormat.ts
+++ src/lib/utils/dateFormat.ts
@@ -20,3 +20,15 @@ export const formatDateTime = (dateString: string): string => {
   // yyyy年mm月dd日 hh時mm分形式に結合
   return `${year}年${month}月${day}日 ${hours}時${minutes}分`;
 };
+
+export const formatDate = (dateString: string): string => {
+  const date = new Date(dateString);
+
+  // 年月日を取得
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+
+  // yyyy年mm月dd日形式に結合
+  return `${year}年${month}月${day}日`;
+};

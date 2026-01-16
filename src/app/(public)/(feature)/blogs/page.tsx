29e1307e6107861a769a8ae1624a3d4e6c02c45b
tofu-dev0123 <m.komukai0123@gmail.com>
2026-01-16 23:19:40 +0900

    ビルドエラーの修正

--- src/app/(public)/(feature)/blogs/page.tsx
+++ src/app/(public)/(feature)/blogs/page.tsx
@@ -1,9 +1,10 @@
+import { Suspense } from 'react';
 import BlogsMain from '@/components/features/public/blogs/BlogsMain';
 
 export default function Page() {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <BlogsMain />
-    </>
+    </Suspense>
   );
 }
